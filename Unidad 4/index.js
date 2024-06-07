9f8b170db4b858e6f8da9ce71d4bf862499bab71
Srezequielr <julianrierawrk@gmail.com>
2024-06-07 00:28:30 -0300

    Algo

---
+++ Unidad 4/index.js
@@ -0,0 +1,18 @@
+var display = document.getElementById("display");
+
+function addToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    var result = eval(display.value);
+    display.value = result;
+  } catch (error) {
+    display.value = "Error";
+  }
+}
