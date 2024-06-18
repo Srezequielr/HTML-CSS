2253182a4262c9f00c13c8e510c3cf4153679e15
Srezequielr <julianrierawrk@gmail.com>
2024-06-18 01:00:24 -0300

    reloj

--- Integrador IPW/index.js
+++ Integrador IPW/index.js
@@ -1,5 +1,5 @@
 function inputHandler() {
-  input = document.querySelectorAll(".inputAdd");
+  let input = document.querySelectorAll(".inputAdd");
   console.log(input);
 
   for (let i = 0; i < input.length; i++) {
@@ -12,4 +12,62 @@ function inputHandler() {
       );
     }
   }
+}
+
+function reloj() {
+  let tiempo = new Date();
+  let hora = tiempo.getHours();
+  let minutos = tiempo.getMinutes();
+  let segundos = tiempo.getSeconds((horas = pad()));
+
+  hora = pad(hora);
+  munitos = pad(minutos);
+  segundos = pad(segundos);
+
+  let horaActual = `${hora}:${munitos}:${segundos}`;
+
+  console.log(horaActual);
+  let horaDiplay = document.getElementById("time");
+  if (horaDiplay != null) {
+    horaDiplay.textContent = horaActual
+  }
+  setTimeout(reloj, 1000);
+}
+
+function pad(number) {
+  return number < 10 ? "0" + number : number;
+}
+
+reloj();
+
+function nameSurnameHandler() {
+  let nameSurname = document.querySelectorAll(".nameSurname");
+  let name = nameSurname[0].value.toUpperCase();
+  let surname = nameSurname[1].value.toUpperCase();
+  let surnameName = document.getElementById("nameSurname");
+  surnameName.innerText = `${surname} ${name}`;
+}
+
+function numTarjHandler() {
+  let numValue = document.querySelector(".inputTarj").value.replace(/ /g, "");
+  document.querySelector(".inputTarj").value = numValue;
+  let num = numValue.match(/.{1,4}/g).join(" ");
+  let numTarj = document.querySelector(".numTarj");
+  numTarj.innerText = num;
+}
+
+function vencHandler() {
+  let vencValue = document
+    .querySelector("#venc")
+    .value.replace(/-/g, "/")
+    .slice(2);
+  let vencDate = document.querySelector("#vencDate");
+  vencDate.innerText = vencValue;
+}
+
+function segHandler() {
+  let vencValue = document.querySelector("#seg").value.replace(/ /g, "");
+  document.querySelector("#seg").value = vencValue;
+  let segCod = document.querySelector(".segCod");
+  segCod.innerText = vencValue;
 }
